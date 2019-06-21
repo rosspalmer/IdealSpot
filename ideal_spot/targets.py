2647d1faff29f68f5237dd9fb9fda3a3e1b37ca8
rosspalmer <rossbpalmer@gmail.com>
2019-06-21 14:42:50 -0600

    add name string identifier for weather target base class

--- ideal_spot/targets.py
+++ ideal_spot/targets.py
@@ -5,12 +5,21 @@ from ideal_spot.data import WeatherDataBase, WeatherFeed
 
 class WeatherTarget(ABC):
 
-    @staticmethod
-    @abstractmethod
-    def calculate_objective(db: WeatherDataBase) -> float:
-        pass
+    def __init__(self, name: str):
+        self.name = name
 
     @staticmethod
     @abstractmethod
     def add_data_to_db(db: WeatherDataBase, new_feed: WeatherFeed) -> WeatherDataBase:
         pass
+
+    @staticmethod
+    @abstractmethod
+    def calculate_objective(db: WeatherDataBase) -> float:
+        pass
+
+    def __eq__(self, other) -> bool:
+        return self.name == other.name
+
+    def __hash__(self) -> int:
+        return hash(self.name)
