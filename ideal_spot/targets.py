f4ce79bfb474e78ad499fe86d986ae776c630e79
rosspalmer <rossbpalmer@gmail.com>
2019-06-22 10:33:44 -0600

    weather target decorator pattern

--- ideal_spot/targets.py
+++ ideal_spot/targets.py
@@ -1,22 +1,58 @@
 from abc import ABC, abstractmethod
+from typing import Dict, Set
 
+from feed import ForecastWeatherFeedFactory, ForecastWeatherFeed
 from optimizer import Spot
 
 
 class WeatherTarget(ABC):
 
-    def __init__(self, name: str):
-        self.name = name
+    def __init__(self):
+        self.forecast_feed = None
 
-    def get_name(self) -> str:
-        return self.name
+    def evaluate_spot(self, spot: Spot):
+        metrics = self.get_forecast_metrics()
+        feed = ForecastWeatherFeedFactory(spot.get_lat(), spot.get_long()).generate_feed(metrics)
+        self.set_forecast_feed(feed)
+        spot.set_scores(self.calculate_scores(spot))
+
+    def get_forecast_feed(self) -> ForecastWeatherFeed:
+        return self.forecast_feed
+
+    def set_forecast_feed(self, forecast_feed: ForecastWeatherFeed):
+        self.forecast_feed = forecast_feed
 
     @abstractmethod
-    def calculate_objective(self, spot: Spot) -> float:
+    def get_forecast_metrics(self) -> Set[str]:
+        return set()
+
+    @abstractmethod
+    def calculate_scores(self, spot: Spot) -> Dict:
         pass
 
-    def __eq__(self, other):
-        return self.name == other.name
 
-    def __hash__(self) -> int:
-        return hash(self.name)
+class WeatherTargetDecorator(WeatherTarget, ABC):
+
+    def __init__(self, target: WeatherTarget, name: str):
+        super().__init__()
+        self.target = target
+        self.name = name
+
+    def get_forecast_feed(self) -> ForecastWeatherFeed:
+        return self.target.get_forecast_feed()
+
+    def set_forecast_feed(self, forecast_feed: ForecastWeatherFeed):
+        self.target.set_forecast_feed(forecast_feed)
+
+    @abstractmethod
+    def get_forecast_metrics(self) -> Set[str]:
+        return self.target.get_forecast_metrics()
+
+    def calculate_scores(self, spot: Spot) -> Dict:
+        scores = self.calculate_scores(spot)
+        scores[self.name] = self._calculate_score(spot)
+        return scores
+
+    @abstractmethod
+    def _calculate_score(self, spot: Spot) -> float:
+        pass
