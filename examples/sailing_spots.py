cca9a703752d21eadf85c545cf337c4af9c9d4ca
rosspalmer <rossbpalmer@gmail.com>
2019-06-23 14:18:59 -0600

    utilize better wind speed values

--- examples/sailing_spots.py
+++ examples/sailing_spots.py
@@ -10,7 +10,7 @@ We will go twice, once right now for eight hours and then
 again twenty fours later at the same spot (guess the
 transporter broke?!). 
 
-For sailing we want about 20 m/s, a temperature of ~295K (22C),
+For sailing we want about 15 m/s, a temperature of ~295K (22C),
 and no precipitation. While temperature and rain are important
 to consider, wind is much more critical.
 """
@@ -22,7 +22,8 @@ spots = {
     Spot('Vancouver', 49.29, -123.29),
     Spot('Montego Bay', 18.47, -77.93),
     Spot('Athens', 37.93, 23.71),
-    Spot('Auckland', -36.84, 174.77)}
+    Spot('Auckland', -36.84, 174.77),
+    Spot('Tokyo', 35.59, 139.86)}
 
 # Get date ranges based on current time
 start_time_first_day = datetime.now()
@@ -32,8 +33,8 @@ end_time_second_day = start_time_second_day + timedelta(hours=8)
 
 # Construct WeatherTarget based on sailing needs
 target = WeatherTarget('79094518408cb847574557c958eeec91')
-target = IdealWindTarget(target, 'wind_day_one', start_time_first_day, end_time_first_day, 20.0)
-target = IdealWindTarget(target, 'wind_day_two', start_time_second_day, end_time_second_day, 20.0)
+target = IdealWindTarget(target, 'wind_day_one', start_time_first_day, end_time_first_day, 15.0)
+target = IdealWindTarget(target, 'wind_day_two', start_time_second_day, end_time_second_day, 15.0)
 target = IdealTempTarget(target, 'temp_day_one', start_time_first_day, end_time_first_day, 295.0)
 target = IdealTempTarget(target, 'temp_day_two', start_time_second_day, end_time_second_day, 295.0)
 target = NewRainTarget(target, 'rain_day_one', start_time_first_day, end_time_first_day)
@@ -44,6 +45,9 @@ score_weights = {'wind_day_one': 2.0, 'wind_day_two': 2.0, 'temp_day_one': 0.5, 
                  'rain_day_one': -0.5, 'rain_day_two': -0.5}
 
 spots = EvaluateSpots.score_spots(spots, target, score_weights)
+score_report = EvaluateSpots.generate_score_report(spots)
+score_report = score_report.sort_values('overall_score', ascending=False)
 
-for spot in spots:
-    print('%s score: %s' % (spot.get_name(), spot.get_overall_score()))
+print(score_report[['name', 'overall_score']])
+print(score_report[['name', 'wind_day_one', 'wind_day_two']])
+print(score_report[['name', 'rain_day_one', 'temp_day_one', 'rain_day_two', 'temp_day_two']])
